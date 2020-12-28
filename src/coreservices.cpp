d893549012b3cfae806c449ec081466b5ee1c8e7
Daniel Schürmann <daschuer@mixxx.org>
2020-12-29 00:58:04 +0100

    Fix leaking controls

--- src/coreservices.cpp
+++ src/coreservices.cpp
@@ -616,6 +616,9 @@ void CoreServices::finalize() {
     ControllerScriptEngineBase::registerTrackCollectionManager(nullptr);
 #endif
 
+    qDebug() << t.elapsed(false).debugMillisWithUnit() << "deleting ScrobblingManager";
+    CLEAR_AND_CHECK_DELETED(m_pScrobblingManager);
+
     // Stop all pending library operations
     qDebug() << t.elapsed(false).debugMillisWithUnit() << "stopping pending Library tasks";
     m_pTrackCollectionManager->stopLibraryScan();
