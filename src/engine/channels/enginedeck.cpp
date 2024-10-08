c80113a67f4f06ee6c12b957e99301dbadc153ef
Antoine C <mixxx@acolombier.dev>
2024-10-08 22:33:38 +0100

    fix: prevent null CO access when cloning sampler or preview

--- src/engine/channels/enginedeck.cpp
+++ src/engine/channels/enginedeck.cpp
@@ -10,6 +10,7 @@
 #include "engine/enginevumeter.h"
 #include "moc_enginedeck.cpp"
 #include "track/track.h"
+#include "util/assert.h"
 #include "util/sample.h"
 
 #ifdef __STEM__
@@ -204,6 +205,18 @@ void EngineDeck::processStem(CSAMPLE* pOut, const int iBufferSize) {
 
 void EngineDeck::cloneStemState(const EngineDeck* deckToClone) {
     VERIFY_OR_DEBUG_ASSERT(deckToClone) {
+        return;
+    }
+    // Sampler and preview decks don't have stem controls
+    if ((m_stemGain.empty() && m_stemMute.empty()) ||
+            (deckToClone->m_stemGain.empty() &&
+                    deckToClone->m_stemMute.empty())) {
+        return;
+    }
+    VERIFY_OR_DEBUG_ASSERT(m_stemGain.size() == kMaxSupportedStems &&
+            m_stemMute.size() == kMaxSupportedStems &&
+            deckToClone->m_stemGain.size() == kMaxSupportedStems &&
+            deckToClone->m_stemMute.size() == kMaxSupportedStems) {
         return;
     }
     for (int stemIdx = 0; stemIdx < kMaxSupportedStems; stemIdx++) {
