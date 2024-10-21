358f66a31bc53ede37b92b3d3368477cfc5a04bd
Antoine C <mixxx@acolombier.dev>
2024-10-21 23:31:12 +0100

    fix: prevent use-after-free with a texture buffer deepcopy

--- src/rendergraph/opengl/texture.cpp
+++ src/rendergraph/opengl/texture.cpp
@@ -1,4 +1,7 @@
 #include "rendergraph/texture.h"
+#include <qnamespace.h>
+#include <qrgb.h>
+#include "rendergraph/assert.h"
 
 #include "rendergraph/context.h"
 
@@ -6,15 +9,23 @@ using namespace rendergraph;
 
 namespace {
 QImage premultiplyAlpha(const QImage& image) {
+    // Since the image is passed by const reference, implicit copy cannot be used, and this Qimage::bits will return a ref the QImage buffer, which may have a shorter lifecycle that the texture buffer. In order to workaround this, and because we cannot copy the image as we need to use the raw bitmap with an explicit image format, we make a manual copy of the buffer
+    QImage texture(image.width(), image.height(), QImage::Format_RGBA8888);
     if (image.format() == QImage::Format_RGBA8888_Premultiplied) {
-        return QImage(image.bits(), image.width(), image.height(), QImage::Format_RGBA8888);
+        VERIFY_OR_DEBUG_ASSERT(texture.sizeInBytes() == image.sizeInBytes()){
+            texture.fill(QColor(Qt::transparent).rgba());
+            return texture;
+        }
+        std::memcpy(texture.bits(), image.bits(), texture.sizeInBytes());
+    } else {
+        auto convertedImage = image.convertToFormat(QImage::Format_RGBA8888_Premultiplied);
+        VERIFY_OR_DEBUG_ASSERT(texture.sizeInBytes() == convertedImage.sizeInBytes()){
+            texture.fill(QColor(Qt::transparent).rgba());
+            return texture;
+        }
+        std::memcpy(texture.bits(), convertedImage.bits(), texture.sizeInBytes());
     }
-    return QImage(
-            image.convertToFormat(QImage::Format_RGBA8888_Premultiplied)
-                    .bits(),
-            image.width(),
-            image.height(),
-            QImage::Format_RGBA8888);
+    return texture;
 }
 } // namespace
 
