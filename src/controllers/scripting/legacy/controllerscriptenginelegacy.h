8b390e4e9b3b71ec1b9969536ec4c1f40e0ac00d
Antoine C. <hi@acolombier.dev>
2026-02-07 22:05:43 +0000

    shared data

--- src/controllers/scripting/legacy/controllerscriptenginelegacy.h
+++ src/controllers/scripting/legacy/controllerscriptenginelegacy.h
@@ -141,4 +141,5 @@ class ControllerScriptEngineLegacy : public ControllerScriptEngineBase {
 
     friend class ControllerScriptEngineLegacyTest;
     friend class MidiControllerTest;
+    friend class ControllerSharedDataTest;
 };
